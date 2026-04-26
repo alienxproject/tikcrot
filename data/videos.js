2903bd844d2ced4d129f8dce4f09938c2784ab17
alienxproject <142660399+alienxproject@users.noreply.github.com>
2026-04-26 07:41:13 +0700

    Create videos.js

---
+++ data/videos.js
@@ -0,0 +1,276 @@
+var videos = [
+  {
+    id: 1,
+    src: "https://cdn.videy.co/23UfokTP1.mp4",
+    thumb: "",
+    title: "dedek suka main bantal ",
+    views: "699k views",
+    duration: "8:32",
+    cta: "https://s.shopee.co.id/8V5E3MrmKp"
+  },
+    {
+    id: 2,
+    src: "https://cdn.videy.co/UtQwWd691.mp4",
+    thumb: "",
+    title: "ponakan nakalin tante",
+    views: "5.1M views",
+    duration: "8:32",
+    cta: ""
+  },
+    {
+    id: 3,
+    src: "https://cdn.videy.co/JMLNaXDM1.mp4",
+    thumb: "",
+    title: "bocil smp sama kaka kelas",
+    views: "2.7M views",
+    duration: "8:32",
+    cta: ""
+  },
+    {
+    id: 4,
+    src: "https://cdn.videy.co/6vDjEMbt1.mp4",
+    thumb: "",
+    title: "masih pink",
+    views: "780K views",
+    duration: "8:32",
+    cta: ""
+  },
+    {
+    id: 5,
+    src: "https://cdn.videy.co/pxowQLTq1.mp4",
+    thumb: "",
+    title: "tete ku bagus ga",
+    views: "903K views",
+    duration: "8:32",
+    cta: ""
+  },
+    {
+    id: 6,
+    src: "https://cdn.videy.co/dOydMjXm1.mp4",
+    thumb: "",
+    title: "tahan jangan sampe keluar",
+    views: "342K views",
+    duration: "8:32",
+    cta: ""
+  },
+    {
+    id: 7,
+    src: "https://cdn.videy.co/V8tqEsVX1.mp4",
+    thumb: "",
+    title: "tetangga kos jago goyang",
+    views: "117K views",
+    duration: "8:32",
+    cta: ""
+  },
+    {
+    id: 8,
+    src: "https://cdn.videy.co/qzKEYw201.mp4",
+    thumb: "",
+    title: "kakak suka ngajakin main pas rumah sepi",
+    views: "761K views",
+    duration: "8:32",
+    cta: ""
+  },
+    {
+    id: 9,
+    src: "https://cdn.videy.co/EPKk7JuT1.mp4",
+    thumb: "",
+    title: "belum pernah ngewe ",
+    views: "6.3M views",
+    duration: "8:32",
+    cta: ""
+  },
+    {
+    id: 10,
+    src: "https://cdn.videy.co/KhegSmFj1.mp4",
+    thumb: "",
+    title: "gede banget sayang ga muat",
+    views: "825K views",
+    duration: "8:32",
+    cta: ""
+  },
+    {
+    id: 11,
+    src: "https://cdn.videy.co/vz8AUgB51.mp4",
+    thumb: "",
+    title: "goyangan wot mu sangat gila",
+    views: "129K views",
+    duration: "5:32",
+    cta: ""
+  },
+    {
+    id: 12,
+    src: "https://cdn.videy.co/zZPinfCq1.mp4",
+    thumb: "",
+    title: "spg magang kena ospek bos",
+    views: "305K views",
+    duration: "20:12",
+    cta: ""
+  },
+  {
+    id: 13,
+    src: "https://cdn.videy.co/TaVkRXJD1.mp4",
+    thumb: "",
+    title: "bungkus pengamen cakep",
+    views: "3.2M views",
+    duration: "6:11",
+    cta: ""
+  },
+  {
+    id: 14,
+    src: "https://cdn.videy.co/Gjo3kdPy1.mp4",
+    thumb: "",
+    title: "bocil sd brutal",
+    views: "534K views",
+    duration: "6:11",
+    cta: ""
+  },
+  
+  {
+    id: 15,
+    src: "https://cdn.videy.co/4PoJrGr31.mp4",
+    thumb: "",
+    title: "pembantu jawa gaada obat",
+    views: "321K views",
+    duration: "6:11",
+    cta: ""
+  },
+  
+  {
+    id: 16,
+    src: "https://cdn.videy.co/BhnrN5Yl1.mp4",
+    thumb: "",
+    title: "dapat bocil facebook",
+    views: "934K views",
+    duration: "6:11",
+    cta: ""
+  },
+  
+  {
+    id: 17,
+    src: "https://cdn.videy.co/G4O76LtQ1.mp4",
+    thumb: "",
+    title: "kakak manfaatin kepolosan adek",
+    views: "2.5M views",
+    duration: "6:11",
+    cta: ""
+  },
+  
+  {
+    id: 18,
+    src: "https://cdn.videy.co/XFQzghKi1.mp4",
+    thumb: "",
+    title: "goyanganya bikin keluar",
+    views: "378K views",
+    duration: "6:11",
+    cta: ""
+  },
+  {
+    id: 19,
+    src: "https://cdn.videy.co/QLlc7SOJ1.mp4",
+    thumb: "",
+    title: "enak banget please",
+    views: "900K views",
+    duration: "6:11",
+    cta: ""
+  },
+    {
+    id: 20,
+    src: "https://cdn.videy.co/IenpIcTN1.mp4",
+    thumb: "",
+    title: "FWB anak twitter",
+    views: "655K views",
+    duration: "6:11",
+    cta: ""
+  },
+  {
+    id: 21,
+    src: "https://cdn.videy.co/SCCLSczc1.mp4",
+    thumb: "",
+    title: "kok bisa enak banget",
+    views: "9.1M views",
+    duration: "6:11",
+    cta: ""
+  },
+  {
+    id: 22,
+    src: "https://cdn.videy.co/Csv3UTKJ1.mp4",
+    thumb: "",
+    title: "padahal masih kecil",
+    views: "871K views",
+    duration: "6:11",
+    cta: ""
+  },
+  {
+    id: 23,
+    src: "https://cdn.videy.co/OYPbsbrL1.mp4",
+    thumb: "",
+    title: "jam istirahat",
+    views: "7.1M views",
+    duration: "6:11",
+    cta: ""
+  },
+  {
+    id: 24,
+    src: "https://cdn.aceimg.com/0e1f681ea.mp4",
+    thumb: "",
+    title: "gas poll maaak",
+    views: "839K views",
+    duration: "6:11",
+    cta: ""
+  },
+  {
+    id: 25,
+    src: "https://cdn.aceimg.com/4d9ffc231.mp4",
+    thumb: "",
+    title: "cindo pulen cute",
+    views: "3.6M views",
+    duration: "6:11",
+    cta: ""
+  },
+  {
+    id: 26,
+    src: "https://cdn2.videy.co/lGyYIQ9f1.mp4",
+    thumb: "",
+    title: "gini kah caranya kak ?",
+    views: "843K views",
+    duration: "6:11",
+    cta: ""
+  },
+  {
+    id: 27,
+    src: "https://cdn.aceimg.com/159639380.mp4",
+    thumb: "",
+    title: "bercanda sama sepupu",
+    views: "6.1M views",
+    duration: "6:11",
+    cta: ""
+  },
+  {
+    id: 28,
+    src: "https://cdn2.videy.co/KXfRAsBh1.mp4",
+    thumb: "",
+    title: "susah punya adek masih sempit",
+    views: "211K views",
+    duration: "6:11",
+    cta: ""
+  },
+  {
+    id: 29,
+    src: "https://cdn.aceimg.com/f6f3f9b7d.mp4",
+    thumb: "",
+    title: "jangan ngomong mama papa",
+    views: "11.5M views",
+    duration: "6:11",
+    cta: ""
+  },
+  {
+    id: 30,
+    src: "https://cdn.videy.co/wbFIgDD81.mp4",
+    thumb: "",
+    title: "pelan pelan aja dek",
+    views: "1.1M views",
+    duration: "6:11",
+    cta: ""
+  }
+];
